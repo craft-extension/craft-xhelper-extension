8d4181bfad7b6e3e10476bcdb5ec44874d01c8bf
Xheldon <c1006044256@gmail.com>
2022-08-14 23:59:53 +0800

    使用自定义的 craftBlockToMarkdown 函数

--- src/utils.ts
+++ src/utils.ts
@@ -52,9 +52,10 @@ export const syncToGithub = async (sync, forceToWechat = false) => {
         const data = result.data.subblocks;
         const title = result.data.content[0].text;
         const coverImage = result.data.style?.coverImage;
-        let markdown = craft.markdown.craftBlockToMarkdown(result.data.subblocks.slice(1), 'common', {
-            tableSupported: true,
-        })
+        // let markdown = craft.markdown.craftBlockToMarkdown(result.data.subblocks.slice(1), 'common', {
+        //     tableSupported: true,
+        // })
+        let markdown = data.slice(1).map(craftBlockToMarkdown).filter(Boolean).join('\n');
         let metaMarkdown = '';
         const metaTable: any = data.slice(0, 1)[0];
         let path = '';
@@ -155,3 +156,162 @@ export const syncToGithub = async (sync, forceToWechat = false) => {
         });
     }
 }
+
+const inlineText = (content = []) => {
+    return content.reduce((pre, curr) => {
+        let result = '';
+        // Note: 顺序影响嵌套规则
+        if (curr.link) {
+            result += '['
+        }
+        if (curr.isCode) {
+            result += '`';
+        }
+        if (curr.isBold) {
+            result += '**';
+        }
+        if (curr.isStrikethrough) {
+            result += '~~';
+        }
+        if (curr.isItalic) {
+            result += '*';
+        }
+        result += curr.text;
+        // Note: 按顺序反着再来一遍
+        if (curr.isItalic) {
+            result += '*';
+        }
+        if (curr.isStrikethrough) {
+            result += '~~';
+        }
+        if (curr.isBold) {
+            result += '**';
+        }
+        if (curr.isCode) {
+            result += '`';
+        }
+        if (curr.link) {
+            result += `](${curr.link.url})`;
+        }
+        return `${pre}${result}`
+    }, '') || ''
+}
+
+export const craftBlockToMarkdown = (block, key, blocks) => {
+    // Note: 优先级最高的是缩进
+    let indent = '';
+    for (let i = 0; i < block.indentationLevel; i++) {
+        indent += `    `;
+    }
+    switch (block.type) {
+        case 'textBlock': {
+            if (!block.content) {
+                console.log(`第 ${key} 个块无内容！`)
+                return;
+            }
+            // Note: title subtitle heading strong body caption 类型
+            //  它们以 block.style.textStyle 区分
+            // Note: page 类型 本身的 block.style.textStyle 为 page
+            //  其 subblocks 也是一个 blocks 数组因此直接递归调用即可；card 同理
+            // Note: 文本类型的加粗、斜体、下划线、行内代码，通过 block.content[x].isBold/isItalic/isStrikethrough/isCode: boolean 区分
+            // Note: textBlock 的引用块通过 hasFocusDecoration/hasBlockDecoration 区分（craft markdown api 只将后者识别为 blockquote）
+            // Note: 离谱的是，todo list 也是 textBlock
+            //  其通过 block.listStyle.type: todo 区分，用 block.listStyle.state: checked/unchecked（取消？） 状态区分
+            //  toggle 通过 block.listStyle.type: toggle 区分，无序列表值为 bullet 有序为 numbered
+            //  list 的层级通过 block.indentationLevel 区分，默认是 0，缩进一次 +1
+            //  textBlock 的 block.content 和  block.subblocks 可能是空
+            //  居中之类的，markdown 不支持，当成最普通的 textBlock 即可，其通过 block.style.alignmentStyle: left center right 区分
+            //  注：如果是引用块，list 的优先级比 blockquote 的优先级高，因此 craft 会将列表写包裹 blockquote： 1. > blockquote content 预期应该是 > 1. blockquote content 才对
+            // Note: 优先判断 block 类型
+            let symbol = '';
+            if (block.listStyle?.type && block.listStyle.type !== 'none') {
+                // Note: blockquote 内可能含 list
+                let blockquote = '';
+                if (block.hasBlockDecoration || block.hasFocusDecoration) {
+                    blockquote = '> '
+                }
+                switch (block.listStyle?.type) {
+                    case 'todo': {
+                        symbol = block.listStyle.state === 'checked' ? '- [x] ' : '- [ ] ';
+                        break;
+                    }
+                    case 'toggle': {
+                        // Note: 我不用 toggle，虽然可以用 html 实现，但是不想
+                        break;
+                    }
+                    case 'bullet': {
+                        symbol = '* '
+                        break;
+                    }
+                    case 'numbered': {
+                        // Note: 可能有多个，但不管多少个，都用同一个 1 进行渲染
+                        symbol = '1. ';
+                    }
+                }
+                return indent + blockquote + symbol + inlineText(block.content) + '\n';
+            }
+            if (block.hasBlockDecoration || block.hasFocusDecoration) {
+                return indent + `> ${inlineText(block.content)}\n`
+            }
+            // Note: 最后渲染为普通的 heading 文本块
+            switch (block.style?.textStyle) {
+                case 'title': {
+                    return `${indent}# ${inlineText(block.content)}\n`;
+                }
+                case 'subtitle': {
+                    return `${indent}## ${inlineText(block.content)}\n`;
+                }
+                case 'heading': {
+                    return `${indent}### ${inlineText(block.content)}\n`;
+                }
+                case 'strong': {
+                    return `${indent}#### ${inlineText(block.content)}\n`;
+                }
+                case 'body': {
+                    return `${indent}${inlineText(block.content)}\n`;
+                }
+                case 'caption': {
+                    return;
+                }
+                case 'page':
+                case 'card': {
+                    return `${indent}${inlineText(block.content)}\n\n${block.subblocks?.map(craftBlockToMarkdown).filter(Boolean).join('\n')}`;
+                }
+            }
+        }
+        case 'horizontalLineBlock': {
+            // Note: 分隔线，通过 block.lineStyle 区分不同样式，值为：extraLight light regular strong
+            return '---\n' 
+        }
+        case 'tableBlock': {
+            // Note: 表格就直接用 api 生成好了，craft 也不支持在表格里面放过多 block
+            return craft.markdown.craftBlockToMarkdown([block], 'common', {
+                tableSupported: true,
+            }) + '\n';
+        }
+        case 'imageBlock': {
+            // Note: 引用的 unsplash 图片，通过 block.url 访问地址，自己上传的一样。区别是 block.previewUrl 前者没值，后者有
+            const next = blocks[key + 1];
+            if (next?.style?.textStyle === 'caption') {
+                return `{% render_caption caption="${next.content[0]?.text || block.filename}" img="${block.url}" %}\n![${block.filename || '__Unsplash__'}](${block.url})\n{% endrender_caption %}\n`
+            }
+            return `![${block.filename || 'Unsplash'}](${block.url})\n`;
+        }
+        case 'urlBlock': {
+            // Note: 将其渲染成 jekyll 自定义标签内容，然后通过自定义插件，构建类似于 Craft、Notion 的 bookmark 的效果
+            return `{% render_bookmark url="${block.url}" title="${block.title || ''}" img="${block.imageUrl || ''}" %}\n${block.pageDescription || ''}\n{% endrender_bookmark %}\n`;
+        }
+        case 'codeBlock': {
+            // Note: 代码块，通过 block.code 为其内容，block.language 为语言
+            // Note: 公式也是该类型，通过 block.language: math_formula 区分
+            if (block.language === 'math_formula') {
+                // Note: markdown 不支持公式
+            }
+            return `\`\`\`${block.language}\n${block.code}\n\`\`\`\n`;
+        }
+        case 'fileBlock': {
+            // 文件不支持
+            return ''
+        }
+    };
+};
